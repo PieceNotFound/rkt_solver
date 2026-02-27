f3d0125827000db91e058a29d6f50b59f03b0b4e
missing <piecenotfound@gmail.com>
2026-02-26 23:37:58 -0500

    refactor: Use itertools

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,5 @@
-use std::fmt::Debug;
-
-use rkt_solver::{
-    MoveOrRot, solve,
-    types::{AxialRotation, Move},
-};
-
-enum Foo {
-    Move(Move),
-    Rot(AxialRotation),
-}
-
-impl Debug for Foo {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Move(arg0) => Debug::fmt(arg0, f),
-            Self::Rot(arg0) => Debug::fmt(arg0, f),
-        }
-    }
-}
-
-impl Foo {
-    fn map_iter(iter: impl Iterator<Item = MoveOrRot>) -> impl Iterator<Item = Self> {
-        iter.flat_map(|v| match v {
-            MoveOrRot::Move(v) => vec![Self::Move(v)],
-            MoveOrRot::Rot(rotation) => rotation.to_axials().map(Self::Rot).collect(),
-        })
-    }
-}
+use itertools::{Either, Itertools};
+use rkt_solver::{MoveOrRot, solve, types::Move};
 
 fn main() {
     let val = std::env::args().nth(1).unwrap();
@@ -36,13 +9,19 @@ fn main() {
         .collect::<Vec<_>>();
     let result = solve(&alg);
     if let Some(result) = result {
-        for (i, v) in Foo::map_iter(result.into_iter()).enumerate() {
-            if i > 0 {
-                print!(" ");
-            }
-            print!("{v:?}");
-        }
-        println!();
+        println!(
+            "{}",
+            result
+                .into_iter()
+                .flat_map(|v| {
+                    match v {
+                        MoveOrRot::Move(mv) => Either::Left(core::iter::once(mv)),
+                        MoveOrRot::Rot(rot) => Either::Right(rot.to_axials()),
+                    }
+                    .factor_into_iter()
+                })
+                .format(" ")
+        );
     } else {
         println!("None");
     }
